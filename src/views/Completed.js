9adef98c388b86318e9df056a07ce4f580661659
albertchitta <albert.w.chittaphong@gmail.com>
2021-11-30 22:25:50 -0600

    Completed setup and navbar

---
+++ src/views/Completed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledCompleted = styled.div``;
+
+export default function Completed() {
+  return (
+    <StyledCompleted>
+      <h1>Completed</h1>
+    </StyledCompleted>
+  );
+}
